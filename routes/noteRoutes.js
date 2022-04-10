4bd949f12036d0b28aee3b334d42ddb214a7dc99
Tabu Hana <taboodog@hotmail.com>
2022-04-10 10:17:13 -0700

    update

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -2,9 +2,19 @@ const router = require('express').Router()
 const { readFile, writeFile } = require('fs')
 const { join, parse } = require('path')
 
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
+})
 
+app.post('/api/notes', (req, res) => {
+  let newNote = {
+    title: req.body.title,
+    text: req.body.text
+  }
 
-
+  notes.push(newNote)
+  res.json(200)
+})
 
 
 
--- server.js
+++ server.js
@@ -10,21 +10,6 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 
-app.get('/api/notes', (req,res) => {
-  res.json(notes)
-})
-
-app.post('/api/notes', (req,res) => {
-  let newNote = {
-    title: req.body.title,
-    text: req.body.text
-  }
-
-  notes.push(newNote)
-  res.json(200)
-})
-
-
 app.use(require('./routes'))
 
 app.listen(process.env.PORT || 3000)
