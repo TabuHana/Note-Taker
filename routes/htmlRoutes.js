0bf5e5bef46ddcfd759c55b13015e625f16e8c4a
Tabu Hana <taboodog@hotmail.com>
2022-04-12 10:03:25 -0700

    fixed html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,12 +4,12 @@ const { join } = require('path')
 
 //Get the notes.html
 router.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'notes.html'))
+  res.sendFile(join(__dirname,'..', 'public', 'notes.html'))
 })
 
 //Get the index.html
 router.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+  res.sendFile(join(__dirname,'..', 'public', 'index.html'))
 })
 
 
